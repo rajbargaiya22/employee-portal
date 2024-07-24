a876a33cb18a09f0e419ffee7d89f2574a180aa6
rajbargaiya22 <rajbargaiya22@gmail.com>
2024-07-24 13:56:46 +0530

    added the ajax search

---
+++ assets/js/rj-custom.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function($) {
+    var searchInput = $('#s');
+    var suggestionsContainer = $('#search-suggestions');
+    var timer;
+
+    searchInput.on('input', function() {
+        clearTimeout(timer);
+        var query = $(this).val();
+
+        if (query.length < 2) {
+            suggestionsContainer.empty();
+            suggestionsContainer.removeClass('show');
+            return;
+        }
+
+        timer = setTimeout(function() {
+            $.ajax({
+                url: ajaxurl,
+                type: 'POST',
+                data: {
+                    action: 'get_search_suggestions',
+                    query: query
+                },
+                success: function(response) {
+                    if (response.success) {
+                        var results = response.data;
+                        var html = '';
+                        
+                        for (var postType in results) {
+                            if (results.hasOwnProperty(postType)) {
+                                html += '<h3>' + capitalizeFirstLetter(postType) + '</h3>';
+                                html += '<ul>';
+                                results[postType].forEach(function(result) {
+                                    html += '<li>';
+                                    html += '<a href="' + result.url + '">';
+                                    html += '<strong>' + result.title + '</strong>';
+                                    // html += '<br><small>' + result.excerpt + '</small>';
+                                    html += '</a>';
+                                    html += '</li>';
+                                });
+                                html += '</ul>';
+                            }
+                        }
+                        
+                        suggestionsContainer.html(html);
+                        suggestionsContainer.addClass('show');
+                    } else {
+                        suggestionsContainer.html('<p>No results found</p>');
+                    }
+                }
+            });
+        }, 300);
+    });
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+});
--- functions.php
+++ functions.php
@@ -7,6 +7,17 @@ function rj_enqueue_styles() {
 
     wp_enqueue_style( 'rj-bootstrap', get_stylesheet_directory_uri(). '/assets/css/bootstrap.css' );
     wp_enqueue_script('rj-bootstrap-js', get_stylesheet_directory_uri(). '/assets/js/bootstrap.js', false, false);
+
+    // wp_enqueue_script('rj-custom-js', get_template_directory_uri() . '/assets/js/rj-custom.js', array('jquery'), null, false);
+	// wp_enqueue_script('jquery');
+	//  wp_localize_script('rj-custom-js', 'ajax_search_params', array(
+	// 		 'ajaxurl' => admin_url('admin-ajax.php'),
+	// 		 'nonce' => wp_create_nonce('ajax-search-nonce'),
+	//  ));
+
+    wp_enqueue_script('rj-custom-js', get_stylesheet_directory_uri() . '/assets/js/rj-custom.js', array('jquery'), '1.0', true);
+    wp_localize_script('rj-custom-js', 'ajaxurl', admin_url('admin-ajax.php'));
+
 }
 
 add_action( 'wp_enqueue_scripts', 'rj_enqueue_styles' );
@@ -25,4 +36,47 @@ require get_stylesheet_directory() . "/admin/rj-post-types.php";
 require get_stylesheet_directory() . "/admin/rj-custom-roles.php";
 require get_stylesheet_directory() . "/admin/rj-company-newsletter-image-box.php";
 require get_stylesheet_directory() . "/admin/rj-new-vendor-meta-box.php";
+
+
+function get_search_suggestions() {
+    $query = sanitize_text_field($_POST['query']);
+    
+    // Get all public post types
+    $post_types = get_post_types(array('public' => true));
+    
+    $results = array();
+    
+    foreach ($post_types as $post_type) {
+        $args = array(
+            's' => $query,
+            'post_type' => $post_type,
+            'posts_per_page' => -1 // Get all matching posts
+        );
+        
+        $search_query = new WP_Query($args);
+        
+        if ($search_query->have_posts()) {
+            $results[$post_type] = array();
+            while ($search_query->have_posts()) {
+                $search_query->the_post();
+                $results[$post_type][] = array(
+                    'title' => get_the_title(),
+                    'url' => get_permalink(),
+                    'excerpt' => wp_trim_words(get_the_excerpt(), 20)
+                );
+            }
+            wp_reset_postdata();
+        }
+    }
+    
+    if (!empty($results)) {
+        wp_send_json_success($results);
+    } else {
+        wp_send_json_error('No results found');
+    }
+}
+add_action('wp_ajax_get_search_suggestions', 'get_search_suggestions');
+add_action('wp_ajax_nopriv_get_search_suggestions', 'get_search_suggestions');
+  
+
 
